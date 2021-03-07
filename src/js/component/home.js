be3707b23a1354929fd1a0ac0244448a20e85063
J Cerdas <jaimecerdas@gmail.com>
2021-03-07 00:00:15 +0000

    Landing page project

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,26 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+import { Jumbotron } from "./jumbotron.js";
+import { NavBar } from "./navBar.js";
+import { Card } from "./card.js";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<NavBar />
+
+			<div className="container justify-content-center">
+				<Jumbotron />
+				<div className="row justify-content-center">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
+			</div>
 		</div>
 	);
 }
